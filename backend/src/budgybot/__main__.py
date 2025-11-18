0a8b0449cfd5e23b0baf463dc3985b8bcdf570bb
Ty Freeman <freemanty27@gmail.com>
2025-11-17 23:02:55 -0500

    Beginning of the Web App transformation

--- backend/src/budgybot/__main__.py
+++ backend/src/budgybot/__main__.py
@@ -1,5 +1,7 @@
 import logging
+import sys
 
+import uvicorn
 from sqlmodel import create_engine, SQLModel, select
 
 from budgybot.configurator import SysConf, get_config
@@ -10,15 +12,19 @@ from budgybot.persistent_models.transactions import ConsumedStatement
 log = logging.getLogger()
 
 
-def main(conf: SysConf) -> None:
+def main(conf: SysConf) -> bool:
     sql_db = f"sqlite:///{str(conf.ledger_dir / conf.ledger_name)}.db"
     printing_press = create_engine(sql_db)
     SQLModel.metadata.create_all(bind=printing_press)
     read_files = fetch(printing_press, select(ConsumedStatement.file_name))
     unread_data = find_records(conf.archive_dir, read_files)
     loop_and_consume(printing_press, unread_data)
-
+    return True
 
 if __name__ == "__main__":
     sys_conf = get_config()
-    main(sys_conf)
+    if main(sys_conf):
+        uvicorn.run("budgybot.api:app", host="0.0.0.0", port=8000, reload=True)
+        sys.exit(0)
+    else:
+        sys.exit(1)
---
+++ backend/src/budgybot/api.py
@@ -0,0 +1,35 @@
+import logging
+
+from sqlmodel import SQLModel, create_engine
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+app = FastAPI(debug=True, title="Budgybot", version="0.1.0")
+sql_path = "sqlite:///backend/budgybot.db"
+records_engine = create_engine(sql_path)
+SQLModel.metadata.create_all(records_engine)
+log = logging.getLogger("budgybot")
+
+origins = [
+        "http://localhost:5173",
+        "http://192.168.50.85:5173",
+]
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Svelte dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+@app.get("/api")
+async def root():
+    return {"message": "Hello World"}
+
+
