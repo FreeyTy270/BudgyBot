80cef9dc97f5ec1d9b05b011923cf3c4fc0dfe9f
Ty Freeman <freemanty27@gmail.com>
2025-11-17 19:18:29 -0500

    Hoist all persistent_models

--- src/budgybot/persistent_models/__init__.py
+++ src/budgybot/persistent_models/__init__.py
@@ -1,1 +1,4 @@
 from budgybot.persistent_models.banks import Bank, BankAccount
+from budgybot.persistent_models.transactions import Transaction, ConsumedStatement
+from budgybot.persistent_models.pay import Salary, Pay
+from budgybot.persistent_models.users import UserProfile
