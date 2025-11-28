24b28cfda19a0f33ea518899020239d81d7ec255
Ty Freeman <freemanty27@gmail.com>
2025-11-28 18:32:55 -0500

    - Moved all record consumption and database addition as methods of BankAccount. - Adds estimated_balance and entry_format as computed_fields of bankaccount - Relationships and database logic work - Overrides __add__ methods of Tranasaction for easier summations - adds already_exists method to check if transaction exists in db. - Adds relationships to ConsumedStatement

--- backend/src/budgybot/persistent_models/banks.py
+++ backend/src/budgybot/persistent_models/banks.py
@@ -1,22 +1,127 @@
-from typing import TYPE_CHECKING
+import logging
+from csv import DictReader
+from datetime import date
+from pathlib import Path
+from functools import cached_property
 
-from sqlmodel import SQLModel, Field, Relationship
+from sqlalchemy import Column
+from pydantic import computed_field, ValidationError, ConfigDict
+from sqlmodel import SQLModel, Field, Relationship, Session
 
-from budgybot.utils.helper_enums import AccountType
+import budgybot.entry_types as etypes
+from budgybot.utils import PathType, AccountType
 
-if TYPE_CHECKING:
-    from budgybot.persistent_models.transactions import Transaction
+from budgybot.persistent_models.transactions import Transaction, ConsumedStatement
 
+
+log = logging.getLogger("budgybot")
 
 class Bank(SQLModel, table=True):
-    name: str = Field(primary_key=True)
+    name: str = Field(primary_key=True, nullable=False, unique=True)
     accounts: list["BankAccount"] = Relationship(back_populates="bank")
 
 
 class BankAccount(SQLModel, table=True):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    
     account_name: str = Field(primary_key=True)
-    bank_name: str = Field(foreign_key="bank.name")
-    bank: "Bank" = Relationship(back_populates="accounts")
     account_type: AccountType = Field(alias="Account Type", nullable=False)
+    archive_dir: Path = Field(default=Path(__file__).parent.parent / "archive", sa_column=Column(PathType))
+
+    ### Relationships ###
+    bank_name: str = Field(foreign_key="bank.name")
+    bank: Bank = Relationship(back_populates="accounts")
+    statements: list["ConsumedStatement"] = Relationship(back_populates="bank_account")
     transactions: list["Transaction"] = Relationship(back_populates="bank_account")
-    estimated_balance: float = Field(alias="Estimated Account Balance")
+
+    @computed_field
+    @property
+    def estimated_balance(self) -> float:
+        return sum(self.transactions)
+
+    @computed_field
+    @cached_property
+    def entry_format(self) -> type[etypes.StatementEntry]:
+        """Uses Bank name and Account type to bind a handle to the entry type of this bank account."""
+        if (self.bank.name.lower() == "chase" and self.account_type is
+                AccountType.CHECKING):
+            fmt = etypes.ChaseCheckingEntry
+        elif (self.bank.name.lower() == "chase" and self.account_type is
+              AccountType.CREDIT):
+            fmt = etypes.ChaseCreditEntry
+        elif (self.bank.name.lower() == "discover" and self.account_type is
+              AccountType.CREDIT):
+            fmt = etypes.DiscoverCreditEntry
+
+        return fmt
+
+
+    def find_records(self) -> list[Path]:
+        """Uses a glob pattern to find records in ``archive_dir``. Returns a list of
+        file paths that are not in the list of ``previously_consumed`` records.
+
+        :return: A list of file paths that have not already been consumed.
+        """
+
+        self_records = self.archive_dir.glob(f"{self.account_name}*.csv",
+                                                   case_sensitive=False)
+
+        new_records = [
+        record
+        for record in self_records
+        if record.stem not in [statement.file_name for statement in self.statements]
+        ]
+
+        return new_records
+
+
+    def consume_csv_record(self, file: Path) -> list[
+        etypes.StatementEntry]:
+        """Reads data in from a csv file located at the Path specified by ``file``. Also
+        updates the data held in the consumed file db.
+
+        :param file: Path to the csv file.
+        :return: A list of Transaction objects.
+        """
+
+        csv_consumed = list()
+
+        with open(file, "r") as f:
+            csv_reader = DictReader(f)
+            for row in csv_reader:
+                if None in row.keys():
+                    row.pop(None)
+
+                try:
+                    row["file_name"] = file.stem
+                    new_entry = self.entry_format(**row)
+                    csv_consumed.append(new_entry)
+                except ValidationError as e:
+                    log.error(
+                        f"Object {self.entry_format} is not complete or properly "
+                        f"formatted\n{e}"
+                    )
+                    raise
+        file_modified_date = date.fromtimestamp(file.stat().st_mtime)
+
+        self.statements.append(ConsumedStatement(
+            file_name=file.stem, report_date=file_modified_date, bank_account=self
+        ))
+
+        return csv_consumed
+
+
+    def update(self, session: Session, list_o_records: list[Path]) -> None:
+        """Loops through a list of records (``list_o_records``) and consumes them into
+        the db.
+        :param session: Active session for interacting with the database.
+        :param list_o_records: List of records to consume.
+        """
+        normalized_entries = list()
+        for record in list_o_records:
+            record_entries = self.consume_csv_record(session, record)
+            for i, entry in enumerate(record_entries):
+                new_bankentry = entry.map_to_bank_entry(self.account_name)
+                if not new_bankentry.already_exists(session):
+                    normalized_entries.append(new_bankentry)
+        self.transactions.extend(sorted(normalized_entries, key=lambda e: e.transaction_date))
--- backend/src/budgybot/persistent_models/transactions.py
+++ backend/src/budgybot/persistent_models/transactions.py
@@ -1,12 +1,18 @@
 from __future__ import annotations
+import logging
 from datetime import date
 from decimal import Decimal
 from typing import TYPE_CHECKING, Optional
 
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field, Relationship, select, and_, Session
+
+from budgybot import records
 
 if TYPE_CHECKING:
     from budgybot.persistent_models.banks import BankAccount
+
+
+log = logging.getLogger("budgybot")
 
 
 # noinspection PyNestedDecorators
@@ -19,6 +25,7 @@ class Transaction(SQLModel, table=True):
     amount: Decimal = Field(alias="Amount")
     description: str = Field(alias="Description")
     transaction_type: str = Field(alias="Transaction Type")
+    notes: Optional[str] = Field(default=None, title="Notes")
 
     bank_account_name: str = Field(foreign_key="bankaccount.account_name")
     bank_account: BankAccount = Relationship(back_populates="transactions")
@@ -33,8 +40,49 @@ class Transaction(SQLModel, table=True):
     #         tx_id = int(desc_split[1])
     #     return tx_id
 
+    """Credit for the __add__ and __radd__ implementations goes to this comment https://discuss.python.org/t/how-to-overload-add-method-in-a-self-made-class-to-sum-multiple-objects-of-the-class/40543/4"""
+    def __add__(self, other):
+        if isinstance(other, Transaction):
+            return self.amount + other.amount
+        elif isinstance(other, int) or isinstance(other, float):
+            return self.amount + other
+        else:
+            raise TypeError("Unsupported operand type(s) for +")
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def already_exists(self, session: Session) -> bool:
+        """Checks whether an entry exists in the database. Used to prevent entries being
+        committed to the database multiple times."""
+
+        exists = False
+        fetched = None
+        fetch_statement = select(Transaction).where(
+            and_(
+                Transaction.description == self.description,
+                Transaction.transaction_date == self.transaction_date,
+                Transaction.amount == self.amount,
+                )
+        )
+        if self.id is None:
+            """This fetch was changed from fetch_one to fetch(all) after discovering an edge case of having multiple
+            valid transactions sharing a date, description, and amount. Program will allow multiple 'identical' transactions
+            when loaded in from the same file by virtue of all entries within a csv file being added to db at the same time
+            """
+            if records.fetch(session, fetch_statement):
+                exists = True
+
+        else:
+            exists = True
+            log.warning(f"Provided Transaction has db id already: {self.id}")
+
+        return exists
+
 
 class ConsumedStatement(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     file_name: str = Field(alias="File Name")
     report_date: date = Field(alias="Report Date")
+    bank_account_name: str = Field(foreign_key="bankaccount.account_name")
+    bank_account: BankAccount = Relationship(back_populates="statements")
