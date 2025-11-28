626b24269b698fe4407959a72c74679832fc567e
Ty Freeman <freemanty27@gmail.com>
2025-11-28 18:37:27 -0500

    Moved session creation up in scope so all record functions now rely on an already open and active session. - Add_single and add_multi are currently deprecated in favor of using native session.add and session.add_all methods. - May convert records functions into basic web api.

--- backend/src/budgybot/records.py
+++ backend/src/budgybot/records.py
@@ -4,71 +4,60 @@ import logging
 from typing import Sequence, TypeVar
 
 import sqlalchemy.exc
-from sqlalchemy import Engine, Select
+from sqlalchemy import Select
 from sqlmodel import SQLModel, Session
 
 _T = TypeVar("_T")
 log = logging.getLogger()
 
 
-def add_single(engine: Engine, item: type[SQLModel]) -> None:
+def add_single(session: Session, item: type[SQLModel]) -> None:
     """Adds a single item to the database."""
-    with Session(engine) as session:
-        log.debug(f"Adding {item} to records")
-        session.add(item)
-        session.commit()
-        log.debug(f"{item} added and committed to records")
-        session.refresh(item)
 
-def add_multi(engine: Engine, items: list[type[SQLModel]]):
+    log.debug(f"Adding {type(item)} to records")
+    session.add(item)
+
+def add_multi(session: Session, items: list[type[SQLModel]]):
     """Adds multiple items to the database."""
-    with Session(engine) as session:
-        session.add_all(items)
-        session.commit()
-        log.debug("Multiple items added and committed to records")
-        log.info("Multiple entries added")
-        for item in items:
-            session.refresh(item)
 
-def remove(engine: Engine, item: type[SQLModel]):
+    session.add_all(items)
+    log.info(f"{len(items)} {type(items[0])} committed to records")
+
+def remove(session: Session, item: type[SQLModel]):
     """Removes a single item from the database."""
-    with Session(engine) as session:
-        session.delete(item)
-        session.commit()
-        log.debug(f"{item} removed from records")
 
-def remove_multi(engine: Engine, items: list[type[SQLModel]]):
+    session.delete(item)
+    log.debug(f"{type(item)} removed from records")
+
+def remove_multi(session: Session, items: list[type[SQLModel]]):
     """Removes multiple items from the database."""
-    with Session(engine) as session:
-        for item in items:
-            session.delete(item)
+    for item in items:
+        session.delete(item)
+    log.debug(f"{len(items)} {type(items[0])} removed from records")
 
-        session.commit()
-        log.debug("Multiple items removed from records")
 
-def fetch(engine: Engine, select_stmt: Select, size: int = 0) -> Sequence[_T]:
+def fetch(session: Session, select_stmt: Select, size: int = 0) -> Sequence[_T]:
     """Fetches ``size`` records from the database. If ``size`` is left at ``0`` the
     function will return all records.
 
-        :param engine: The database engine to use.
+        :param session: The active db session to use.
         :param select_stmt: The select statement to execute.
         :param size: The number of records to fetch. Defaults to 0.
         :returns: A list of records.
     """
 
-    with Session(engine) as session:
-        records_result = session.exec(select_stmt)
-        if size > 0:
-            records = records_result.fetchmany(size=size)
-        else:
-            records = records_result.fetchall()
+    records_result = session.exec(select_stmt)
+    if size > 0:
+        records = records_result.fetchmany(size=size)
+    else:
+        records = records_result.fetchall()
 
     return records
 
-def fetch_one(engine: Engine, select_stmt: Select, okay_if_none: bool = True) -> _T:
+def fetch_one(session: Session, select_stmt: Select, okay_if_none: bool = True) -> _T:
     """Fetches a single record from the database.
 
-        :param engine: The database engine to use.
+        :param session: The active db session to use.
         :param select_stmt: The select statement to execute.
         :param okay_if_none: If ``True``, fetches using ``one_or_none()`` allowing for
         the fetch to return ``None`` without throwing an exception. Defaults to ``True``.
@@ -76,17 +65,16 @@ def fetch_one(engine: Engine, select_stmt: Select, okay_if_none: bool = True) ->
     """
     record = None
 
-    with Session(engine) as session:
-        try:
-            if okay_if_none:
-                record = session.exec(select_stmt).one_or_none()
-            else:
-                record = session.exec(select_stmt).one()
-        except sqlalchemy.exc.MultipleResultsFound:
-            num_records = len(session.exec(select_stmt).all())
-            log.warning(f"{num_records} records found where 1 was expected")
-        except sqlalchemy.exc.NoResultFound:
-            log.error("No records were found where 1 was needed")
-            ## TODO: Create custom exception behavior when necessary
+    try:
+        if okay_if_none:
+            record = session.exec(select_stmt).one_or_none()
+        else:
+            record = session.exec(select_stmt).one()
+    except sqlalchemy.exc.MultipleResultsFound:
+        num_records = len(session.exec(select_stmt).all())
+        log.warning(f"{num_records} records found where 1 was expected")
+    except sqlalchemy.exc.NoResultFound:
+        log.error("No records were found where 1 was needed")
+        ## TODO: Create custom exception behavior when necessary
 
     return record
